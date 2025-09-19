d6b9d43a4dd04cddca09ac9e8ef89cfc163cf411
Diego Riveros <65474383+noitsdiego@users.noreply.github.com>
2025-09-19 15:52:16 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend funcionando en Railway");
+});
+
 app.use("/tasks", taskRoutes);
 app.use("/songs", songRoutes);
 
-const PORT = process.env.PORT || 4000;
+// 👇 Railway asigna el puerto en la variable de entorno PORT
+const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server  running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
